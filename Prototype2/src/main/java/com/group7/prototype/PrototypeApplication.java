e63e1c8d0d46612c1f97835b5548c144c7730a1a
Tas5743 <73266212+Tas5743@users.noreply.github.com>
2020-10-23 17:37:19 -0400

    Add files via upload

---
+++ Prototype2/src/main/java/com/group7/prototype/PrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.group7.prototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypeApplication.class, args);
+    }
+
+}
---
+++ Prototype2/src/test/java/com/group7/prototype/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7.prototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
