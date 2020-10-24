7691284864f7c587cddf3f2d368a7bfb9d2792bb
Scionzenos <73364702+Scionzenos@users.noreply.github.com>
2020-10-24 17:16:18 -0400

    cart is duplicate of manager view

--- Prototype2/src/main/java/com/group7/prototype/PrototypeApplication.java
+++ Prototype2/src/main/java/com/group7/prototype/PrototypeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PrototypeApplication {
-
     public static void main(String[] args) {
         System.getProperties().put( "server.port", 8081);
         SpringApplication.run(PrototypeApplication.class, args);
